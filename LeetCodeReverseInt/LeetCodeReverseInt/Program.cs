534e9cce117ddd07499d744a1511281d31b4da5f
Kevin <kevnaf@gmail.com>
2018-09-18 13:16:42 -0700

    int reverse problem

---
+++ LeetCodeReverseInt/LeetCodeReverseInt/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace LeetCodeReverseInt
+{
+    //Given a 32-bit signed integer, reverse digits of an integer.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstTest = 132;
+            int secondTest = -132;
+            int thirdTest = 1534236469;
+
+            Console.WriteLine(ReversInt(firstTest));
+            Console.ReadLine();
+            Console.WriteLine(ReversInt(secondTest));
+            Console.ReadLine();
+            Console.WriteLine(ReversInt(thirdTest));
+            Console.ReadLine();
+        }
+
+        public static int ReversInt(int input)
+        {
+            //Convert input to a string so we can manipulate it as an array
+            string inputString = input.ToString();
+            //init our output string
+            string outputString = "";
+            //Set our loop to pull the last digit and put it in front of our output string
+            for (int i = inputString.Length - 1; i >= 0; i--)
+            {
+                if (inputString[i].ToString() == "-")
+                {
+                    outputString = inputString[i].ToString() + outputString;
+                }
+                else
+                {
+                    outputString = outputString + inputString[i].ToString();
+                }
+            }
+            int answerInt;
+            //Try to parse the string into an int
+            bool canParse = int.TryParse(outputString, out answerInt);
+            //If successful return int if not return 0
+            if (canParse)
+            {
+                return answerInt;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+       //Researched method
+       public static int BetterReverse(int input)
+        {
+            int answerInt = 0;
+            while (input != 0)
+            {
+                //Pull off our last digit using mod 10
+                int step = input % 10;
+                //Reduce our input
+                input /= 10;
+                //If our answer is greater than the int max / 10 it will overflow
+                //If our answer is = to our max value and our step value is > 7 it will also overflow
+                if (answerInt > int.MaxValue / 10 || (answerInt == int.MaxValue / 10 && step > 7)) return 0;
+                //Same logic for the negative side
+                if (answerInt < int.MinValue / 10 || (answerInt == int.MinValue / 10 && step > -8)) return 0;
+                //Add our step value to our answer value
+                answerInt = answerInt * 10 + step;
+            }
+            return answerInt;
+        }
+    }
+}
---
+++ ListWorkOne/ListWorkOne/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListWorkOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        //Given the numTrips and the possibleTrips and coordinateness, Find the shortest numTrips. Return a list of initial coordinateness.
+        public List<List<int>> BestTrips(int numTrips,
+                                             List<int[,]> coordinateness,
+                                             int possibleTrips)
+        {
+
+        }
+
+        //Given deviceCapacity find all combinations of optimal foregroundAppList and backgroundAppList.
+        //A pair is optimal if their is not another pair that sums to a greater value but is still less than the device capacity
+        public List<List<int>> optimalUtilization(int deviceCapacity,
+                                             List<List<int>> foregroundAppList,
+                                             List<List<int>> backgroundAppList)
+        {
+            //Set our comparable max
+            int max = 0;
+            //Make our empty list if we found no applicable results
+            List<List<int>> answerList = new List<List<int>>();
+            //Check each forgroundApp against each backgroundApp
+            for (int i = 0; i < foregroundAppList.Count; i++)
+            {
+                for (int k = 0; k < backgroundAppList.Count; k++)
+                {
+                    //If we find a matching pair add it to our list and record its total memory value
+                    int memUsage = foregroundAppList[i][1] + backgroundAppList[k][1];
+                    if (memUsage <= deviceCapacity)
+                    {
+                        if (foregroundAppList[i][1] + backgroundAppList[k][1] >= max)
+                        {
+                            answerList.Add(new List<int> { foregroundAppList[i][0], backgroundAppList[k][0] });
+                            max = foregroundAppList[i][1] + backgroundAppList[k][1];
+                        }
+                    }
+                }
+            }
+            return answerList;
+        }
+    }
+}
